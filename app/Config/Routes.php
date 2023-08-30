093b55d4f2f756cd549ce38c32206ad80ffffa5d
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-08-30 16:01:46 +0700

    add: routes, views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,5 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/test', 'Home::test');
+$routes->get('/hello', 'Hello::index');
+$routes->get('/say_hello', 'Hello::say_hello');
--- app/Views/v_index.php
+++ app/Views/v_index.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
+    <title>Index</title>
 </head>
-
 <body>
-
-    <?php
-    echo $name;
-    ?>
-
+    <H3>Ini adalah halaman index Hello</H3>
 </body>
-
 </html>
